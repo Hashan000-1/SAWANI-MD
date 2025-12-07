05bf49b7e3706b3cff7e18fd350f1c97f0a5f07e
Hashan000-1 <pabasarahashan977@gmail.com>
2025-12-07 19:25:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,6 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "HUtlzIKY#x-cgq9mrajvvRwtP89IeOt5_OLlSUZPt86JRO57g3Zo",
 ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "true",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/l06cb8.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> "
 };
